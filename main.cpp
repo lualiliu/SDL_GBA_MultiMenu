980505205c5da7308ee8ebc1460a7153be4bbad7
lualiliu <lualiliu@outlook.com>
2024-03-26 21:37:52 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,3 @@
-/*This source code copyrighted by Lazy Foo' Productions (2004-2013)
-and may not be redistributed without written permission.*/
 //The headers
 #include "SDL/SDL.h"
 #include "SDL/SDL_image.h"
